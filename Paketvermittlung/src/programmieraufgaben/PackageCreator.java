dbd124cbf72b8ebdb3d3a0560ea7d9ffac35524d
Kassem Abbas <kassem_abbas6@outlook.com>
2020-11-23 02:54:33 +0100

    updated

--- Paketvermittlung/src/programmieraufgaben/PackageCreator.java
+++ Paketvermittlung/src/programmieraufgaben/PackageCreator.java
@@ -1,5 +1,6 @@
 package programmieraufgaben;
 
+import javax.xml.crypto.Data;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -95,12 +96,9 @@ public class PackageCreator {
 
             }
         }
-        System.out.println(dataPackage.getNachricht());
         dataPackage.setNachricht(dataPackage.getNachricht().replaceAll("^[\n\r]", "").replaceAll("^[\n\r]", ""));
         dataPackage.setNachricht(dataPackage.getNachricht().trim());
         dataPackage.setNachricht(dataPackage.getNachricht().replaceAll("[\n\r]", "\\\\n"));
-
-        System.out.println(dataPackage.getNachricht());
 
         return dataPackage;
     }
@@ -123,11 +121,47 @@ public class PackageCreator {
                     return null;
                 }
             }else{
-                if(arrOfStr[i].length()>dataPackage.getDataPackageLength()){
+                if(arrOfStr[i].replaceAll("\\W", "").length() >dataPackage.getDataPackageLength() ){
                     System.out.println("Die Nachricht kann nicht versendet werden, da sie ein Wort mit Laenge " +
                             arrOfStr[i].length()+" > " + dataPackage.getDataPackageLength() + " enthaelt.");
                     return null;
                 }
+            }
+        }
+        int add = -1;
+        for (int j = 0; j<arrOfStr.length; j++){
+            if(arrOfStr[j].length()<=dataPackage.getDataPackageLength()){
+                DataPackage element = new DataPackage(arrOfStr[j].length());
+                element.setNachricht(arrOfStr[j]);
+                dataPackages.add(element);
+                add++;
+                for(int k = j+1; k<arrOfStr.length; k++){
+                    if(dataPackages.get(add).getNachricht().length() + 1 + arrOfStr[k].length() <= dataPackage.getDataPackageLength()){
+                        dataPackages.get(add).setNachricht(dataPackages.get(add).getNachricht() + " " + arrOfStr[k]);
+                    }else {
+                        if(arrOfStr[k].contains("\\n") &&
+                                dataPackages.get(add).getNachricht().length() + 1 + arrOfStr[k].length() -2 <=dataPackage.getDataPackageLength()){
+                            arrOfStr[k] = arrOfStr[k].replaceAll("\\\\n","");
+                            dataPackages.get(add).setNachricht(dataPackages.get(add).getNachricht() + " " + arrOfStr[k]);
+                            arrOfStr[k+1] = "\\n" + arrOfStr[k+1];
+                        }else{
+                            j = k-1;
+                            break;
+                        }
+                    }
+                }
+            }
+            else{
+                if(arrOfStr[j].contains("\\n") && arrOfStr[j].length() -2 <=dataPackage.getDataPackageLength()){
+                    arrOfStr[j] = arrOfStr[j].replaceAll("\\\\n","");
+                    DataPackage element2 = new DataPackage(arrOfStr[j].length());
+                    element2.setNachricht(arrOfStr[j]);
+                    dataPackages.add(element2);
+                    add++;
+                    arrOfStr[j+1] = "\\n" + arrOfStr[j+1];
+
+                }
+
             }
         }
 
@@ -141,7 +175,9 @@ public class PackageCreator {
      * @param dataPackages Hier wird die Liste uebergeben, deren Elemente in die Kommandozeile ausgegeben werden sollen
      */
     public void printOutPackage(List<DataPackage> dataPackages) {
-
+        for(int i = 0; i<dataPackages.size(); i++){
+            System.out.println(dataPackages.get(i).getNachricht());
+        }
     }
 }
 
