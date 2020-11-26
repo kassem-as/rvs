f9d083052aecbdc80016bcf1f3e66ec1733c8826
Kassem Abbas <kassem_abbas6@outlook.com>
2020-11-26 02:43:39 +0100

    updated

--- Paketvermittlung/src/programmieraufgaben/PackageCreator.java
+++ Paketvermittlung/src/programmieraufgaben/PackageCreator.java
@@ -1,6 +1,8 @@
 package programmieraufgaben;
 
-import javax.xml.crypto.Data;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -17,63 +19,44 @@ public class PackageCreator {
      * @return Gibt das als Parameter uebergebene Objekt, dass mit den abgefragten Werten befuellt wurde zurueck
      */
     public DataPackage fillParameters(DataPackage dataPackage) {
-        System.out.print("Version : ");
-        dataPackage.setVersion(input.nextInt());
-        if(dataPackage.getVersion()!=4 && dataPackage.getVersion()!=6)
-        {
-            System.out.println("Version ungueltig. Bitte waehlen Sie entweder Version 4 oder Version 6.");
-            return null;
-        }
 
-        System.out.print("Absender : ");
-        dataPackage.setSender(input.next());
+        System.out.print("Version : "); dataPackage.setVersion(input.nextInt());
+        if(dataPackage.getVersion()!=4 && dataPackage.getVersion()!=6) { System.out.
+                println("Version ungueltig. Bitte waehlen Sie entweder Version 4 oder Version 6."
+                ); return null; }
+
+        System.out.print("Absender : "); dataPackage.setSender(input.next());
 
         if(dataPackage.getSender() == null) {
-            System.out.println("Bitte geben sie eine richtige ipv4");
-            return null;
-        }
+            System.out.println("Bitte geben sie eine richtige ipv4"); return null; }
 
-        String [] ipSender;
-        if(dataPackage.getVersion()==4) {
-            ipSender = dataPackage.getSender().split("\\.");
+        String [] ipSender; if(dataPackage.getVersion()==4) { ipSender =
+                dataPackage.getSender().split("\\.");
 
 
             if(ipSender.length < 4|| ipSender.length >4) {
-                System.out.println("Bitte geben sie eine richtige format des IPv4");
-                return null;
-            }
-        }else {
-            ipSender = dataPackage.getSender().split("\\.");
+                System.out.println("Bitte geben sie eine richtige format des IPv4"); return
+                        null; } }else { ipSender = dataPackage.getSender().split("\\.");
             if(ipSender.length < 6 || ipSender.length >6 ) {
-                System.out.println("Bitte geben sie eine richtige format des IPv6");
-                return null;
-            }
-        }
+                System.out.println("Bitte geben sie eine richtige format des IPv6"); return
+                        null; } }
 
-        System.out.print("Empfaenger : ");
-        dataPackage.setEmpfaenger(input.next());
+        System.out.print("Empfaenger : "); dataPackage.setEmpfaenger(input.next());
 
         if(dataPackage.getSender() == null) {
-            System.out.println("Bitte geben sie eine richtige ipv4");
-            return null;
-        }
+            System.out.println("Bitte geben sie eine richtige ipv4"); return null; }
 
-        String [] ipReceiver;
-        if(dataPackage.getVersion()==4) {
-            ipReceiver = dataPackage.getEmpfaenger().split("\\.");
+        String [] ipReceiver; if(dataPackage.getVersion()==4) { ipReceiver =
+                dataPackage.getEmpfaenger().split("\\.");
 
 
             if(ipReceiver.length < 4|| ipReceiver.length >4) {
-                System.out.println("Bitte geben sie eine richtige format des IPv4");
-                return null;
-            }
-        }else {
-            ipReceiver = dataPackage.getEmpfaenger().split("\\.");
+                System.out.println("Bitte geben sie eine richtige format des IPv4"); return
+                        null; } }else { ipReceiver = dataPackage.getEmpfaenger().split("\\.");
             if(ipReceiver.length < 6 || ipReceiver.length >6 ) {
-                System.out.println("Bitte geben sie eine richtige format des IPv6");
-                return null;
-            }
-        }
+                System.out.println("Bitte geben sie eine richtige format des IPv6"); return
+                        null; } }
+
 
         String sanad = "";
         String str = "";
@@ -87,21 +70,22 @@ public class PackageCreator {
             }
 
             if(sanad.isEmpty()){
-                
+
                 dataPackage.setNachricht((dataPackage.getNachricht() + "\n"));
             }
             else {
 
-                dataPackage.setNachricht(dataPackage.getNachricht() + sanad +"\n" + " ");
+                dataPackage.setNachricht(dataPackage.getNachricht() + " " + sanad + " " +"\n" + " ");
 
             }
         }
         dataPackage.setNachricht(dataPackage.getNachricht().replaceAll("^[\n\r]", "").replaceAll("^[\n\r]", ""));
         dataPackage.setNachricht(dataPackage.getNachricht().trim());
         dataPackage.setNachricht(dataPackage.getNachricht().replaceAll("[\n\r]", "\\\\n"));
-
+        dataPackage.setNachricht(dataPackage.getNachricht().trim());
         return dataPackage;
     }
+
 
     /**
      * Aus dem als Parameter uebergebenen Paket sollen die Informationen
@@ -110,63 +94,59 @@ public class PackageCreator {
      * @param dataPackage Hier wird das Objekt uebergeben in das das Resultat gespeichert werden soll
      * @return Gibt das als Parameter uebergebene Objekt mit den aufgeteiltet Datenpaketen zurueck
      */
+
+    public int checkLength (String input, String extension, DataPackage dataPackage) {
+        if(extension.equals("\\n") || input.endsWith("\\n")) {
+            if((input.length() + extension.length())<=dataPackage.getDataPackageLength()) {
+                return 1; //fall /n und geht
+            }else {
+                return 0; //nicht geht
+            }
+        }else {
+            if((input.length() + extension.length() + 1)<=dataPackage.getDataPackageLength()) {
+                return 2; //fall nicht \n und geht
+            }else {
+                return 0; // nicht geht
+            }
+        }
+    }
+
+
     public List<DataPackage> splitPackage(DataPackage dataPackage) {
         List<DataPackage> dataPackages = new LinkedList<>();
-        String[] arrOfStr = dataPackage.getNachricht().split(" ");
-        for(int i = 0; i< arrOfStr.length; i++){
-            if(arrOfStr[i].contains("\\n")){
-                if(arrOfStr[i].length() - 2 > dataPackage.getDataPackageLength()){
-                    System.out.println("Die Nachricht kann nicht versendet werden, da sie ein Wort mit Laenge " +
-                                    arrOfStr[i].length()+" > " + dataPackage.getDataPackageLength() + " enthaelt.");
-                    return null;
-                }
-            }else{
-                if(arrOfStr[i].replaceAll("\\W", "").length() >dataPackage.getDataPackageLength() ){
-                    System.out.println("Die Nachricht kann nicht versendet werden, da sie ein Wort mit Laenge " +
-                            arrOfStr[i].length()+" > " + dataPackage.getDataPackageLength() + " enthaelt.");
-                    return null;
-                }
-            }
-        }
-        int add = -1;
-        for (int j = 0; j<arrOfStr.length; j++){
-            if(arrOfStr[j].length()<=dataPackage.getDataPackageLength()){
-                DataPackage element = new DataPackage(arrOfStr[j].length());
-                element.setNachricht(arrOfStr[j]);
-                dataPackages.add(element);
-                add++;
-                if(j== arrOfStr.length-1){ break;}
-                for(int k = j+1; k<arrOfStr.length; k++){
-                    if(dataPackages.get(add).getNachricht().length() + 1 + arrOfStr[k].length() <= dataPackage.getDataPackageLength()){
-                        dataPackages.get(add).setNachricht(dataPackages.get(add).getNachricht() + " " + arrOfStr[k]);
-                    }else {
-                        if(arrOfStr[k].contains("\\n") &&
-                                dataPackages.get(add).getNachricht().length() + 1 + arrOfStr[k].length() -2 <=dataPackage.getDataPackageLength()){
-                            arrOfStr[k] = arrOfStr[k].replaceAll("\\\\n","");
-                            dataPackages.get(add).setNachricht(dataPackages.get(add).getNachricht() + " " + arrOfStr[k]);
-                            arrOfStr[k+1] = "\\n" + arrOfStr[k+1];
-                        }else{
-                            j = k-1;
-                            break;
-                        }
+
+        String [] result = dataPackage.getNachricht().split("( )");
+
+        for(int i=0; i<result.length; i++) {
+            String input=result[i];
+            for(int j=i+1; j<result.length; j++) {
+                String extension = result[j];
+                if(checkLength(input, extension, dataPackage) == 1) {
+                    input = input + extension;
+                    if(j==(result.length-1)) {
+                        i = j;
                     }
+                }else if(checkLength(input, extension, dataPackage) == 2) {
+                    input = input + " " + extension;
+                    if((j==result.length-1)) {
+                        i = j;
+                    }
+                }else {
+                    i=j-1;
+                    break;
                 }
             }
-            else{
-                if(arrOfStr[j].contains("\\n") && arrOfStr[j].length() -2 <=dataPackage.getDataPackageLength()){
-                    arrOfStr[j] = arrOfStr[j].replaceAll("\\\\n","");
-                    DataPackage element2 = new DataPackage(arrOfStr[j].length());
-                    element2.setNachricht(arrOfStr[j]);
-                    dataPackages.add(element2);
-                    add++;
-                    arrOfStr[j+1] = "\\n" + arrOfStr[j+1];
-                }
-            }
+            DataPackage newPackage= new DataPackage(input.length());
+            newPackage.setNachricht(input);
+            newPackage.setEmpfaenger(dataPackage.getEmpfaenger());
+            newPackage.setSender(dataPackage.getSender());
+            newPackage.setVersion(dataPackage.getVersion());
+            dataPackages.add(newPackage);
         }
-
-
         return dataPackages;
     }
+
+
 
     /**
      * Diese Methode gibt den Inhalt der empfangenen Pakete in der Komandozeile aus
@@ -174,9 +154,20 @@ public class PackageCreator {
      * @param dataPackages Hier wird die Liste uebergeben, deren Elemente in die Kommandozeile ausgegeben werden sollen
      */
     public void printOutPackage(List<DataPackage> dataPackages) {
+        System.out.println();
+        System.out.println("Es sind " + dataPackages.size() + " Datenpakete notwendig.");
+        System.out.println();
         for(int i = 0; i<dataPackages.size(); i++){
-            System.out.println(dataPackages.get(i).getNachricht());
+            dataPackages.get(i).setPacketLaufNummer(i+1);
+            System.out.println("Version: " +dataPackages.get(i).getVersion());
+            System.out.println("Absender: " +dataPackages.get(i).getSender());
+            System.out.println("Empfaenger: " +dataPackages.get(i).getEmpfaenger());
+            System.out.println("Paketlaufnummer: " +dataPackages.get(i).getPacketLaufNummer());
+            System.out.println("Datenteil-Laenge: " +dataPackages.get(i).getDataPackageLength());
+            System.out.println("Datenteil: " +dataPackages.get(i).getNachricht());
+            System.out.println();
+
         }
+
     }
 }
-
